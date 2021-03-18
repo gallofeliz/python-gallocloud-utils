2886505ebb66ed06b3494a0054bd8ac9acd6f0ab
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2021-03-18 21:03:54 +0100

    added fn

--- schedule.py
+++ schedule.py
@@ -1,7 +1,9 @@
-import time
+import time, threading, sched
 from croniter import croniter
 
-def get_next_schedule_time(schedule_or_schedules, now = time.time()):
+def get_next_schedule_time(schedule_or_schedules, now = None):
+    if not now:
+        now = time.time()
     if type(schedule_or_schedules) is not list:
         schedule_or_schedules = [schedule_or_schedules]
 
@@ -17,3 +19,69 @@ def get_next_schedule_time(schedule_or_schedules, now = time.time()):
     times = list(map(_lambda, schedule_or_schedules))
 
     return round(sorted(times)[0])
+
+def schedule_once(schedule_or_schedules, fn, args = (), kwargs={}, on_error = None):
+    def _action():
+        try:
+            fn(*args, **kwargs)
+        except Exception as e:
+            if on_error:
+                on_error(e)
+            else:
+                raise e
+
+    scheduler = sched.scheduler(time.time)
+    evt = scheduler.enterabs(get_next_schedule_time(schedule_or_schedules), 1, _action)
+    scheduler.run()
+
+    return lambda: scheduler.cancel(evt)
+
+def schedule(schedule_or_schedules, fn, args = (), kwargs={}, on_error = None):
+    evt = None
+
+    def _action():
+        _schedule_next()
+        try:
+            fn(*args, **kwargs)
+        except Exception as e:
+            if on_error:
+                on_error(e)
+            else:
+                raise e
+
+    def _schedule_next():
+        evt = scheduler.enterabs(get_next_schedule_time(schedule_or_schedules), 1, _action)
+
+    scheduler = sched.scheduler(time.time)
+    _schedule_next()
+    scheduler.run()
+
+    return lambda: scheduler.cancel(evt)
+
+def schedule_once_in_thread(schedule_or_schedules, fn, args = (), kwargs={}, on_error = None):
+    threading.Thread(
+        target=schedule_once,
+        kwargs={
+            'schedule_or_schedules': schedule_or_schedules,
+            'fn': fn,
+            'args': args,
+            'kwargs': kwargs,
+            'on_error': on_error
+        }
+    ).start()
+
+    #return lambda: raise Exception('Not handled')
+
+def schedule_in_thread(schedule_or_schedules, fn, args = (), kwargs={}, on_error = None):
+    threading.Thread(
+        target=schedule,
+        kwargs={
+            'schedule_or_schedules': schedule_or_schedules,
+            'fn': fn,
+            'args': args,
+            'kwargs': kwargs,
+            'on_error': on_error
+        }
+    ).start()
+
+    #return lambda: raise Exception('Not handled')
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import unittest
 import schedule
+import threading
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
+    def test_get_next_schedule_time(self):
         now = 1616086475
         print('now', now)
         print('each 10s, should be 1616086485', schedule.get_next_schedule_time('10s', now))
@@ -17,6 +18,20 @@ class TestStringMethods(unittest.TestCase):
             '* * * * sun',
             '0 * * * thu',
         ], now))
+
+    def test_schedule_once(self):
+        def my_action(name):
+            if name == 'Paul':
+                raise Exception('Bad name')
+            print('hello ' + name)
+
+        def on_error(e):
+            print('Error handled', e)
+
+        schedule.schedule_once('1s', my_action, args = ('Jean', ), kwargs={}, on_error =on_error)
+        schedule.schedule_once('1s', my_action, args = ('Paul', ), kwargs={}, on_error =on_error)
+
+        schedule.schedule_once_in_thread('1s', my_action, args = ('Jean', ), kwargs={}, on_error =on_error)
 
 if __name__ == '__main__':
     unittest.main()
