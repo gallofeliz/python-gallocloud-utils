063fb0420558db94a0d20a42adc7b4b8e59bce5d
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2021-04-30 00:47:57 +0200

    refacto encapsulated def

--- gallocloud_utils/scheduling.py
+++ gallocloud_utils/scheduling.py
@@ -1,15 +1,16 @@
 import time, threading, sched
 from croniter import croniter
 
+def convert_to_seconds(duration):
+    seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
+    return int(duration[:-1]) * seconds_per_unit[duration[-1].lower()]
+
 def get_next_schedule_time(schedule_or_schedules, now = None):
     if not now:
         now = time.time()
     if type(schedule_or_schedules) is not list:
         schedule_or_schedules = [schedule_or_schedules]
 
-    def convert_to_seconds(duration):
-        seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
-        return int(duration[:-1]) * seconds_per_unit[duration[-1].lower()]
 
     def _lambda(schedule):
         if ' ' in schedule:
